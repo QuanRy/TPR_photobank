9b8c17e42a7ddf8caa9610dcabfbde9cb09b0252
QuanRy <kacman1964@mail.ru>
2024-12-20 00:35:59 +0300

    Проверим и сделаем первый commit черрез VSCode.

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. (first commit)
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.PositiveIntegerField()
