02f5df60ca8ac56e38e21dfacfd7d0702d9007c3
QuanRy <kacman1964@mail.ru>
2025-01-10 00:29:17 +0300

    получилось сделать фильтрацию, но картинки при пролистывании заканчиваются и остается лишь одна слева и она меняется просто

--- shop/views.py
+++ shop/views.py
@@ -6,5 +6,22 @@ from django.shortcuts import render
 from .models import Photo
 
 def photo_list(request):
+    # Хэштеги для каждой категории
+    new_year_tags = ['#праздник', '#новыйгод', '#рождество']
+    summer_tags = ['#лето']
+    cities_tags = ['#город']
+
+    # Получаем все фотографии
     photos = Photo.objects.all()
-    return render(request, 'shop/index.html', {'photos': photos})
+
+    # Фильтрация по категориям с проверкой хэштегов
+    new_year_photos = [photo for photo in photos if any(tag in photo.get_hashtags_list() for tag in new_year_tags)]
+    summer_photos = [photo for photo in photos if any(tag in photo.get_hashtags_list() for tag in summer_tags)]
+    cities_photos = [photo for photo in photos if any(tag in photo.get_hashtags_list() for tag in cities_tags)]
+
+    # Отправляем в шаблон
+    return render(request, 'shop/index.html', {
+        'new_year_photos': new_year_photos,
+        'summer_photos': summer_photos,
+        'cities_photos': cities_photos,
+    })
