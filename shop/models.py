9ac23a6a50dafb02831a0ac385371ee66c7caa8e
QuanRy <kacman1964@mail.ru>
2025-01-09 22:15:28 +0300

    пока хэштег передаются по одному

--- shop/models.py
+++ shop/models.py
@@ -2,15 +2,16 @@ from django.db import models
 
 class Hashtag(models.Model):
     name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
     
 class Photo(models.Model):
     image_path = models.CharField(max_length=255)
     description = models.TextField()
-    hashtags = models.ManyToManyField(Hashtag)
+    hashtags = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     
     class Meta:
         db_table = 'shop_photo'
+
+    def get_hashtags_list(self):
+        """Возвращает список хэштегов"""
+        return [tag.strip() for tag in self.hashtags.split(",")]
